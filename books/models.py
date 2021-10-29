05620bc72d5f9b01bfc0e75722b310256c3c3412
angelinalis <angelina.lis18@gmail.com>
2021-10-29 18:01:58 +0300

    change Book model

--- books/models.py
+++ books/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from django import forms
 
 class Book(models.Model):
-    author = models.CharField(max_length=124, verbose_name='Автор', unique=True)
+    author = models.CharField(max_length=124, verbose_name='Автор')
     author_birth_year = models.PositiveIntegerField(verbose_name='Год рождения автора')
     book_written_year = models.PositiveIntegerField(verbose_name='Год издания книги')
-    book_title = models.CharField(max_length=124, db_index=True, verbose_name='Название книги', unique=True)
+    book_title = models.CharField(max_length=124, db_index=True, verbose_name='Название книги')
 
     def __str__(self):
         return self.book_title
